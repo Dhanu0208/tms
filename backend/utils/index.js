913532b6c9369b5f5e5fa85e9d8406f21d4f953a
Dhanu0208 <dhanu@metazonetechnologies.com>
2025-01-31 11:58:16 +0530

    backend setup
    added middlewares
    
    created database and it's connection

---
+++ backend/utils/index.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const dbConnection = async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      port: process.env.MYSQL_PORT || 3306,
+    });
+
+    console.log("✅ MySQL Database Connected Successfully");
+
+    return connection;
+  } catch (error) {
+    console.error("❌ MySQL Connection Error:", error.message);
+    process.exit(1);
+  }
+};
+
+export const createJWT = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  // Change sameSite from strict to none when you deploy your app
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict", //prevent CSRF attack
+    maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
+  });
+};
